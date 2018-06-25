b3276b62b3d66ac018794639c07e36d495c86b27
Fadilla Maulida <31851579+fmdilla@users.noreply.github.com>
2018-06-25 14:29:23 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=Z4GjAIAdAAuSxHCRYXLNw1nVSm9LMGajlKLKYo2R";
         
         $("#loadImage").click(function(){
             $.ajax({
